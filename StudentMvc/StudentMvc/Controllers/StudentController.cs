581d0bee223d8c03d14c587509fd03454d8f420a
Iva Petrovic <iva.petrovic@infobip.com>
2019-01-19 16:00:56 +0100

    Created Edit screen for StudentMvc project

--- StudentMvc/StudentMvc/Controllers/StudentController.cs
+++ StudentMvc/StudentMvc/Controllers/StudentController.cs
@@ -31,7 +31,10 @@ namespace StudentMvc.Controllers
                     item.LastName = row["LastName"].ToString();
                     item.FirstName = row["FirstName"].ToString();
                     item.City = row["City"].ToString();
-                    item.Age = int.Parse(row["Age"].ToString());
+                    if(!String.IsNullOrEmpty(row["Age"].ToString()))
+                    {
+                        item.Age = int.Parse(row["Age"].ToString());
+                    }
                     itemsList.Add(item);
                 }
             }
@@ -78,6 +81,64 @@ namespace StudentMvc.Controllers
                     cmd.CommandText = "DELETE FROM dbo.Student WHERE Id = @id";
 
                     cmd.Parameters.AddWithValue("@id", studentId);
+
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        return RedirectToAction("Index");
+                    }
+                    catch (SqlException e)
+                    {
+                        return View();
+                    }
+                }
+            }
+        }
+
+        public ActionResult Edit(int studentId)
+        {
+            Student student = new Student();
+            using (SqlConnection conn = new SqlConnection(mvcDbConnectionString))
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM dbo.Student WHERE Id = @id;", conn);
+                command.Parameters.AddWithValue("@id", studentId);
+                conn.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        student.Id = int.Parse(reader["Id"].ToString());
+                        student.FirstName = reader["FirstName"].ToString();
+                        student.LastName = reader["LastName"].ToString();
+                        student.City = reader["City"].ToString();
+                        if (!String.IsNullOrEmpty(reader["Age"].ToString()))
+                        {
+                            student.Age = int.Parse(reader["Age"].ToString());
+                        }
+                        return View(student);
+                    }
+                }
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            using (SqlConnection conn = new SqlConnection(mvcDbConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "UPDATE dbo.Student SET FirstName = @firstName, LastName = @lastName, Age = @age, City = @city WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", student.Id);
+                    cmd.Parameters.AddWithValue("@firstName", student.FirstName);
+                    cmd.Parameters.AddWithValue("@lastName", student.LastName);
+                    cmd.Parameters.AddWithValue("@age", student.Age);
+                    cmd.Parameters.AddWithValue("@city", student.City);
 
                     try
                     {
